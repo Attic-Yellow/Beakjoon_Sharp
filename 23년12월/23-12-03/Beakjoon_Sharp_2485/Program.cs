a7913a256173b2c1a8ad9d9a7369df5f996cec2c
loftdoggy <chtori1202@gmail.com>
2023-12-03 19:35:49 +0900

    백준 2485 '가로수' 문제 해결 :
    - 인접한 가로수 간의 간격 계산 및 최대공약수로 추가 가로수 수 계산

--- 23년12월/23-12-03/Beakjoon_Sharp_2485/Program.cs
+++ 23년12월/23-12-03/Beakjoon_Sharp_2485/Program.cs
@@ -14,8 +14,45 @@ using System;
 
 class Program
 {
+
+    // 유클리드 호제법을 이용해 최대공약수를 구하는 함수
+    static int GCD(int a, int b)
+    {
+        if (b == 0)
+            return a;
+        else
+            return GCD(b, a % b);
+    }
+
     static void Main(string[] args)
     {
+        int N = int.Parse(Console.ReadLine());
+        int gcd = 0;
+        int answer = 0;
+        List<int> treeList = new List<int>();
+        List<int> intervalList = new List<int>();
+        
+        // 가로수의 위치를 입력받습니다.
+        for (int i = 0; i < N; i++)
+        {
+            treeList.Add(int.Parse(Console.ReadLine()));
+        }
 
+        gcd = treeList[1] - treeList[0];
+
+        // 인접한 가로수 사이의 간격을 계산하고 최대공약수를 구합니다.
+        for (int i = 1; i < N - 1; i++)
+        {
+            int interval = treeList[i + 1] - treeList[i];
+            gcd = GCD(gcd, interval);
+        }
+
+        // 필요한 추가 가로수의 수를 계산합니다.
+        for (int i = 0; i < N - 1; i++)
+        {
+            int interval = treeList[i + 1] - treeList[i];
+            answer += interval / gcd - 1;
+        }
+        Console.WriteLine(answer);
     }
 }
